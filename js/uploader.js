6629a0b51520f8b64ec879c553427ad8fba5e228
cmonsalt <cristiantm12.com>
2022-02-06 19:26:26 -0500

    uploader con AJAX

---
+++ js/uploader.js
@@ -0,0 +1,35 @@
+const d = document,
+  $main = d.querySelector("main"),
+  $files = d.getElementById("files");
+
+const uploader = (file) => {
+  //console.log(file);
+  const xhr = new XMLHttpRequest(),
+    formData = new FormData();
+
+  formData.append("file", file);
+
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) {
+      return;
+    }
+
+    if (xhr.status >= 200 && xhr.status < 300) {
+      console.log(xhr.responseText);
+    } else {
+      let message = xhr.statusText || "ocurrio un error";
+      err(`Error ${xhr.status}: ${message}`);
+    }
+  });
+  xhr.open("POST", "assets/uploader.php");
+  xhr.setRequestHeader("enc-type", "multipart/form-data");
+  xhr.send(formData);
+};
+
+d.addEventListener("change", (e) => {
+  if (e.target === $files) {
+    //console.log(e.target.files);
+    const files = Array.from(e.target.files);
+    files.forEach((el) => uploader(el));
+  }
+});
